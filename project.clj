6d1f135996463557eee437eb19bccf443f80fbe6
Daniel Ciumberică <ciumbi.ionut@gmail.com>
2021-11-21 11:37:32 -0500

    Updated deps and build config

--- project.clj
+++ project.clj
@@ -3,14 +3,16 @@
   :url "https://github.com/nighcoder/widgets"
   :license {:name "MIT"}
 
-  :dependencies [[org.clojars.nighcoder/clojupyter "0.4.1"]
+  :dependencies [[clojupyter/clojupyter "0.4.0-alpha0"]
                  [camel-snake-kebab "0.4.1"]
                  [cheshire "5.10.0"]
                  [io.simplect/compose "0.7.27"]]
 
 
-  :profiles {:dev           {:plugins [[org.clojars.nighcoder/lein-metajar "0.1.2"]]}
+  :profiles {:dev           {:plugins [[org.clojars.nighcoder/lein-metajar "0.1.3"]]}
              :metajar       {:libdir "../lib"
-                             :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}}
+                             :jvm-opts ["-Dclojure.compiler.direct-linking=true"]
+                             :target-path "target/plugins"}
+             :provided      {:dependencies [[clojupyter/clojupyter "0.4.0-alpha0"]]}}
 
-  :target-path "target/plugins")
+  :target-path "target")
